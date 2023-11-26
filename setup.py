f465bcdc4eeae15ef16582b0b931030681f47f51
sandeep Github <sandeeprairai339@gmail.com>
2023-11-26 15:23:30 +0530

    Cookicutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project will recommend top 3 food on swiggy home page',
+    author='Sandeep kumar',
+    license='',
+)
---
+++ src/data/make_dataset.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+# 1.Reading data from csv
+
+def read_csv(file_path):
+    return pd.read_csv(file_path)
